7a33feb3b1e10d8f65e0ad90ded03afd41621769
NachoSallad <samuel.castenstrom@yahoo.se>
2021-12-08 12:52:07 +0100

    fler blackjack funktioner tillagda

--- lib/blackjack.dart
+++ lib/blackjack.dart
@@ -1,12 +1,19 @@
+import 'package:my_first_app/game_page.dart';
 import 'package:playing_cards/playing_cards.dart';
 
 import 'deck_of_cards.dart';
 
 class BlackJack {
   List<PlayingCard> playerHand = <PlayingCard>[];
+  List<PlayingCard> splitHand = <PlayingCard>[];
   List<PlayingCard> dealerHand = <PlayingCard>[];
+  int playerBet = 0;
+  int splitBet = 0;
+  bool doubled = false;
+  bool split = false;
 
   void startNewGame() {
+    //här skapas ett nytt spel och startkort delas ut, ska kanske ha något med bet att göra
     playerHand.clear();
     dealerHand.clear();
 
@@ -14,5 +21,73 @@ class BlackJack {
     dealerHand.add(DeckOfCards().pickACard());
     playerHand.add(DeckOfCards().pickACard());
     dealerHand.add(DeckOfCards().pickACard());
+  }
+
+  void dealersTurn() {
+    if (DeckOfCards().handValue(dealerHand) < 17) {
+      dealerHand.add(DeckOfCards().pickACard());
+      //lägg till funktion för vinst och förlust attribut
+    } else {
+      //lägg till funktion för at stanna
+    }
+  }
+
+  bool increaseBet(int bet) {
+    //ökar spelarens insats
+    if (bet <= saldo) {
+      playerBet = bet;
+      saldo -= bet;
+      return true;
+    } else if (bet > saldo) {
+      return false;
+    } else {
+      return false;
+    }
+  }
+
+  bool getNewCard() {
+    //drar ett nytt kort för spelaren
+    if (!doubled) {
+      playerHand.add(DeckOfCards().pickACard());
+      return true;
+    } else if (doubled && playerHand.length < 3) {
+      playerHand.add(DeckOfCards().pickACard());
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool doDouble() {
+    //en dubblering av insatsen
+    if (saldo >= playerBet) {
+      saldo -= playerBet;
+      playerBet = playerBet * 2;
+      doubled = true;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool doSplit() {
+    //gör en split om det väljs och kraven uppfylls
+    if (DeckOfCards().valueOfCard(playerHand[0]) ==
+            DeckOfCards().valueOfCard(playerHand[1]) &&
+        saldo >= playerBet) {
+      splitHand.add(playerHand[1]);
+      playerHand.removeAt(1);
+
+      splitBet = playerBet;
+      saldo -= splitBet;
+      playerHand.add(DeckOfCards().pickACard());
+      splitHand.add(DeckOfCards().pickACard());
+
+      split = true;
+      return true;
+    } else {
+      split = false;
+      return false;
+    }
   }
 }
--- lib/deck_of_cards.dart
+++ lib/deck_of_cards.dart
@@ -1,13 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:playing_cards/playing_cards.dart';
 import 'dart:math';
 
 class DeckOfCards {
   List<PlayingCard> deck = standardFiftyTwoCardDeck(); //ska den ligga här?
-  var random = Random();
+  final _random = Random();
 
   PlayingCard pickACard() {
     //returnerar ett kort och tar bort det ur leken
-    int i = random.nextInt(deck.length + 1);
+    int i = _random.nextInt(deck.length + 1);
     PlayingCard card = deck[i];
     deck.removeAt(i);
     return card;
@@ -16,5 +17,72 @@ class DeckOfCards {
   void resetDeck() {
     //resettar kortleken
     deck = standardFiftyTwoCardDeck();
+  }
+
+  int valueOfCard(PlayingCard card) {
+    //returnerar värdet av ett specifikt kort
+    switch (card.value) {
+      case CardValue.ace:
+        {
+          return 1;
+        }
+      case CardValue.two:
+        {
+          return 2;
+        }
+      case CardValue.three:
+        {
+          return 3;
+        }
+      case CardValue.four:
+        {
+          return 4;
+        }
+      case CardValue.five:
+        {
+          return 5;
+        }
+      case CardValue.six:
+        {
+          return 6;
+        }
+      case CardValue.seven:
+        {
+          return 7;
+        }
+      case CardValue.eight:
+        {
+          return 8;
+        }
+      case CardValue.nine:
+        {
+          return 9;
+        }
+      case CardValue.ten:
+        {
+          return 10;
+        }
+      case CardValue.jack:
+        {
+          return 10;
+        }
+      case CardValue.queen:
+        {
+          return 10;
+        }
+      case CardValue.king:
+        {
+          return 10;
+        }
+    }
+  }
+
+  int handValue(List<PlayingCard> hand) {
+    //returnerar värdet av en "hand" (lista av kort)
+    int value = 0;
+    for (PlayingCard card in hand) {
+      value += valueOfCard(card);
+    }
+    return value;
   }
 }
