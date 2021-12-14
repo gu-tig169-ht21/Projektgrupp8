74e657925731289cb8416b2d973ee0c28a3bb6b2
mogge1997 <joakimsundahl1997@gmail.com>
2021-12-14 15:40:34 +0100

    lagt till knappar på settings

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_switch/flutter_switch.dart';
+import 'card_customization.dart';
 
 class Settings extends StatefulWidget {
   const Settings({Key? key}) : super(key: key);
@@ -7,29 +10,93 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
-  bool darkThemeValue = true;
+  bool darkThemeValue = false;
+  bool soundValue = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Settings"),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [],
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("Settings"),
+          centerTitle: true,
+        ),
+        body: Stack(
+          children: [
+            _darkTheme(),
+            _sound(),
+            _cardCustomizationButton(),
+            //_settingsDarkTheme(),
+            //  _settingsLightTheme(),
+          ],
+        ));
   }
 
-  void _buildSettingsDarkTheme() {
-    //return ThemeMode.dark;
+/*
+  Widget buildItem(String item) {
+    return ListTile(
+      title: const Text('Darktheme'),
+      leading: FlutterSwitch(
+          width: 125.0,
+          height: 55.0,
+          valueFontSize: 25.0,
+          toggleSize: 45.0,
+          value: darkThemeValue,
+          borderRadius: 30.0,
+          padding: 8.0,
+          showOnOff: true,
+          onToggle: (val) {
+            setState(() {
+              darkThemeValue = val;
+            });
+          }),
+    );
+  }*/
 
-    /*if (darkThemeValue == true){
-    return ThemeData(DarkTheme);
-    }
-    else {
-      return ThemeData(StandardTheme);
-      }*/
+  Widget _darkTheme() {
+    return Align(
+        alignment: const Alignment(0.3, 0.1),
+        child: FractionallySizedBox(
+            widthFactor: 0.2,
+            heightFactor: 0.1,
+            child: SwitchListTile(
+                title: const Text('Dark Theme'),
+                value: darkThemeValue,
+                onChanged: (bool value) {
+                  setState(() {
+                    darkThemeValue = value;
+                  });
+                })));
+  }
+
+  Widget _sound() {
+    return Align(
+        alignment: const Alignment(0.3, 0.3),
+        child: FractionallySizedBox(
+            widthFactor: 0.2,
+            heightFactor: 0.1,
+            child: SwitchListTile(
+                title: const Text('Sound'),
+                value: soundValue,
+                onChanged: (bool value) {
+                  setState(() {
+                    soundValue = value;
+                  });
+                })));
+  }
+
+  Widget _cardCustomizationButton() {
+    return Align(
+        alignment: const Alignment(0.2, 0.9),
+        child: FractionallySizedBox(
+            widthFactor: 0.3,
+            heightFactor: 0.1,
+            child: ElevatedButton(
+                child: const Text('Card Customization'),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const CustomizationPage()));
+                })));
   }
 }
