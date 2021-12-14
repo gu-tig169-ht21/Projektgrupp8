e25e1da4ab3aa5270df88edb31b15b800af2e219
mogge1997 <joakimsundahl1997@gmail.com>
2021-12-14 17:43:51 +0100

    ändrat utseendet på knapparna

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -54,12 +54,18 @@ class _SettingsState extends State<Settings> {
 
   Widget _darkTheme() {
     return Align(
-        alignment: const Alignment(0.3, 0.1),
+        alignment: const Alignment(0.3, -0.4),
         child: FractionallySizedBox(
-            widthFactor: 0.2,
+            widthFactor: 0.35,
             heightFactor: 0.1,
             child: SwitchListTile(
                 title: const Text('Dark Theme'),
+                secondary: const Icon(Icons.dark_mode),
+                controlAffinity: ListTileControlAffinity.leading,
+                activeColor: Colors.black,
+                activeTrackColor: Colors.grey,
+                inactiveThumbColor: Colors.white,
+                inactiveTrackColor: Colors.white10,
                 value: darkThemeValue,
                 onChanged: (bool value) {
                   setState(() {
@@ -70,15 +76,20 @@ class _SettingsState extends State<Settings> {
 
   Widget _sound() {
     return Align(
-        alignment: const Alignment(0.3, 0.3),
+        alignment: const Alignment(0.2, -0.1),
         child: FractionallySizedBox(
-            widthFactor: 0.2,
+            widthFactor: 0.3,
             heightFactor: 0.1,
             child: SwitchListTile(
                 title: const Text('Sound'),
+                secondary: const Icon(Icons.volume_off_sharp),
+                controlAffinity: ListTileControlAffinity.leading,
+                activeColor: Colors.green,
+                activeTrackColor: Colors.teal[50],
                 value: soundValue,
                 onChanged: (bool value) {
                   setState(() {
+                    //lägga till en icon för när ljudet är unmutat
                     soundValue = value;
                   });
                 })));
@@ -86,17 +97,24 @@ class _SettingsState extends State<Settings> {
 
   Widget _cardCustomizationButton() {
     return Align(
-        alignment: const Alignment(0.2, 0.9),
+        alignment: const Alignment(0, 0.9),
         child: FractionallySizedBox(
             widthFactor: 0.3,
             heightFactor: 0.1,
             child: ElevatedButton(
-                child: const Text('Card Customization'),
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const CustomizationPage()));
-                })));
+              child: const Text('Card Customization'),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const CustomizationPage()));
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green[200],
+                onPrimary: Colors.black87,
+                textStyle:
+                    const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+            )));
   }
 }
