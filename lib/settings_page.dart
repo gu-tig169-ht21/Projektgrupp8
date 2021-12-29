9d50ebf4c613d7f20bc5b282e9d5f5ef1ec3fc81
louise <louise.grankarlsson@gmail.com>
2021-12-29 11:58:12 +0100

    ändrat layout listview

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -18,106 +18,123 @@ class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Settings"),
-        ),
-        body: ListView(
-          children: [
-            _numberOfDecks(),
-            _darkTheme(),
-            _sound(),
-            _cardCustomizationButton(),
-            //_settingsDarkTheme(),
-            //  _settingsLightTheme(),
-          ],
-        ));
-  }
-
-  Widget _darkTheme() {
-    return Align(
-      alignment: const Alignment(0.3, -0.4),
-      child: SwitchListTile(
-          title: const Text(
-            'Dark Theme',
-          ),
-          subtitle: const Text('Sets the theme to dark'),
-          secondary: const Icon(Icons.dark_mode),
-          controlAffinity: ListTileControlAffinity.trailing,
-          activeColor: Colors.green,
-          activeTrackColor: Colors.green[200],
-          value: Provider.of<ChangeTheme>(context, listen: true)
-              .getThemeModeSwitch,
-          onChanged: (bool? value) {
-            Provider.of<ChangeTheme>(context, listen: false)
-                .changeDarkTheme(value!);
-          }
-
-          // provider istället för setState
-          //thememode set till darktheme när man trycker på knappen
-
-          ),
+      appBar: AppBar(
+        title: const Text("Settings"),
+      ),
+      body: ListView(
+        children: [
+          _numberOfDecks(),
+          _darkTheme(),
+          _sound(),
+          _cardCustomizationButton()
+          //_settingsDarkTheme(),TA BORT??
+          //  _settingsLightTheme(),TA BORT????
+        ],
+      ),
     );
   }
 
-  Widget _sound() {
-    return Align(
-        alignment: const Alignment(0.2, -0.1),
-        child: SwitchListTile(
-            title: const Text('Sound'),
-            subtitle: const Text('Turns sound off'),
-            secondary: const Icon(Icons.volume_off_sharp),
-            controlAffinity: ListTileControlAffinity.trailing,
-            activeColor: Colors.green,
-            activeTrackColor: Colors.green[200],
-            value: soundValue,
-            onChanged: (bool value) {
-              //lägga till en icon för när ljudet är unmutat
-              soundValue = value;
-            }));
+//widget för att ändra temat dark/standard
+  Widget _darkTheme() {
+    return SwitchListTile(
+        title: const Text(
+          'Dark Theme',
+        ),
+        subtitle: const Text('Sets the theme to dark'),
+        secondary: const Icon(Icons.dark_mode),
+        controlAffinity: ListTileControlAffinity.trailing,
+        activeColor: Colors.green,
+        activeTrackColor: Colors.green[200],
+        value:
+            Provider.of<ChangeTheme>(context, listen: true).getThemeModeSwitch,
+        onChanged: (bool? value) {
+          Provider.of<ChangeTheme>(context, listen: false)
+              .changeDarkTheme(value!);
+        }
+
+        // provider istället för setState
+        //thememode set till darktheme när man trycker på knappen
+
+        );
   }
 
+//widget för ljudet av/på
+  Widget _sound() {
+    return SwitchListTile(
+      title: const Text('Sound'),
+      subtitle: const Text('Turns sound off'),
+      secondary: const Icon(Icons.volume_off_sharp),
+      controlAffinity: ListTileControlAffinity.trailing,
+      activeColor: Colors.green,
+      activeTrackColor: Colors.green[200],
+      value: soundValue,
+      onChanged: (bool value) {
+        //lägga till en icon för när ljudet är unmutat
+        soundValue = value;
+      },
+    );
+  }
+
+//widget för knapp till --> card customization
+  // Widget _cardCustomizationButton() {
+  //   return ElevatedButton(
+  //     child: const Text('Card Customization'),
+  //     onPressed: () {
+  //       Navigator.push(context,
+  //           MaterialPageRoute(builder: (context) => const CustomizationPage()));
+  //     },
+  //     // style: ElevatedButton.styleFrom(
+  //     //   textStyle: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+  //     // ),
+  //   );
+  // }
   Widget _cardCustomizationButton() {
-    return Align(
-        alignment: const Alignment(0, 0.8),
-        child: ElevatedButton(
-          child: const Text('Card Customization'),
-          onPressed: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => const CustomizationPage()));
-          },
-          style: ElevatedButton.styleFrom(
-            //   primary: Colors.green[200],
-            //   onPrimary: Colors.black87,
-            textStyle:
-                const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-          ),
-        ));
+    return ListTile(
+      leading: const Icon(Icons.brush),
+      title: const Text('Card Customization'),
+      subtitle: const Text('Change card theme'),
+      trailing: IconButton(
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const CustomizationPage()));
+        },
+        icon: const Icon(
+          Icons.east,
+        ),
+      ),
+    );
   }
 }
 
 Widget _numberOfDecks() {
-  return Consumer<ChangeNumberOfDecks>(
-    builder: (context, state, child) {
-      return DropdownButton<String>(
-        value: Provider.of<ChangeNumberOfDecks>(context, listen: false)
-            .getSelectedNumberOfDecks,
-        onChanged: (String? newValue) {
-          Provider.of<ChangeNumberOfDecks>(context, listen: false)
-              .setSelectedDecks(newValue!);
-        },
-        items: Provider.of<ChangeNumberOfDecks>(context, listen: false)
-            .getNumberDecks
-            .map<DropdownMenuItem<String>>((String value) {
-          return DropdownMenuItem<String>(
-            value: value,
-            child: Text(value),
+  return ListTile(
+      leading: const Icon(Icons.add),
+      title: const Text(
+        'Number of decks',
+      ),
+      subtitle: const Text('Sets the number of decks'),
+      trailing: Consumer<ChangeNumberOfDecks>(
+        builder: (context, state, child) {
+          return DropdownButton<String>(
+            value: Provider.of<ChangeNumberOfDecks>(context, listen: false)
+                .getSelectedNumberOfDecks,
+            onChanged: (String? newValue) {
+              Provider.of<ChangeNumberOfDecks>(context, listen: false)
+                  .setSelectedDecks(newValue!);
+            },
+            items: Provider.of<ChangeNumberOfDecks>(context, listen: false)
+                .getNumberDecks
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
           );
-        }).toList(),
-      );
-    },
-  );
+        },
+      ));
 }
 
 class ChangeNumberOfDecks with ChangeNotifier {
